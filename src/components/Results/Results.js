16a1c2e530d7ce0f64b286b462d1a55387560871
Swayamsvk <swayam@qristix.com>
2022-01-29 18:57:30 +0530

    added results page

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 function Results() {
   const { state } = useLocation();
-  const renObjData = state.map(function (data, id) {
+  const [prevData, setPrevData] = useState(state);
+  useEffect(() => {
+    //console.log(prevData, "this is prevdata");
+    // setPrevData(
+    //   prevData.map((d) => {
+    //     return {
+    //       rank: d.rank,
+    //       id: d.id,
+    //       username: d.username,
+    //       password: d.password,
+    //     };
+    //   })
+    // );
+  }, []);
+  //console.log(prevData, "this is prevdata");
+
+  const showRank = state.map(function (data, id) {
     if (data.rank == 1) {
       return <p key={id}>{data.dishName} with Rank 1</p>;
     }
@@ -15,9 +31,41 @@ function Results() {
     }
     // return <p key={id}>{data}</p>;
   });
+  const renObjData = prevData.map(function (curData, id) {
+    //console.log(curData, "curData is this");
+    return (
+      <p key={id}>
+        {curData.dishName}
+        <input
+          placeholder="Rank"
+          className="field"
+          type="text"
+          value={curData.rank}
+          onChange={(e) => {
+            let curValue = e.target.value;
+            setPrevData(
+              prevData.map((newData) => {
+                if (curData.id == newData.id) {
+                  newData.rank = curValue;
+                }
+                return newData;
+              })
+            );
+          }}
+          required
+        />
+      </p>
+    );
+  });
 
-  console.log(state, "this is result");
-  return <div>{renObjData}</div>;
+  //console.log(state, "this is result");
+  return (
+    <div>
+      {showRank}
+      <br />
+      {renObjData}
+    </div>
+  );
 }
 
 export default Results;
