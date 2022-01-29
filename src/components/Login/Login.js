7b00a64350e0178fcf2fe60b263d8d66ebb2ce83
Swayamsvk <swayam@qristix.com>
2022-01-30 02:17:14 +0530

    redirect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,34 +6,6 @@ import Typography from "@mui/material/Typography";
 import { Grid, Paper, TextField, Button } from "@mui/material";
 
 const Login = (props) => {
-  // const User = [
-  //   {
-  //     id: 1,
-  //     username: "amar",
-  //     password: "amar123",
-  //   },
-  //   {
-  //     id: 2,
-  //     username: "akbar",
-  //     password: "akbar123",
-  //   },
-  //   {
-  //     id: 3,
-  //     username: "antony",
-  //     password: "antony123",
-  //   },
-  //   {
-  //     id: 4,
-  //     username: "john",
-  //     password: "john123",
-  //   },
-  //   {
-  //     id: 5,
-  //     username: "paul",
-  //     password: "paul123",
-  //   },
-  // ];
-
   useEffect(() => {
     document.body.style.backgroundColor = "#DCDCDC";
   }, []);
